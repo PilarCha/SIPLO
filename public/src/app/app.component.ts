1f246e62cc6578ee7a9ab773fc44080c14c64b8d
Pilar Chavez <pchavez92@gmail.com>
2018-02-17 12:16:18 -0800

    Beer mug shows up on every search

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -7,7 +7,8 @@ import {ApiService} from './api.service'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  show:boolean=false;
+  showBars:boolean=false;
+  showSearch: boolean=false;
   error:string;
   words:string;
   newPlace:object;
@@ -24,47 +25,57 @@ export class AppComponent {
       location:''
     },
     this.words=" ",
-    this.show=false
+    this.showBars=false,
+    this.showSearch=false
   }
   ngOnInit() {
     this.getword()
     this.getLocation()
   }
   getword(){
-    var words=["Lips that touch liquor touch other lips quicker…","I drink to make other people more interesting.", "Everybody has to believe in something. I believe I’ll have another drink.", "You picked the wrong time to quit drinking", "It's always happy hour somewhere in the world"]
+    var words=["Lips that touch liquor touch other lips quicker…","I drink to make other people more interesting.", "Everybody has to believe in something. I believe I’ll have another drink.", "You picked the wrong time to quit drinking", "It's always happy hour somewhere in the world", "This one is on the house"]
     this.words=words[Math.floor(Math.random()*(words.length))]
   }
   onClick(object){
     var address=object.location[0]+object.location[1]+object.name
-    window.open("https://maps.google.com/?q="+address)
+    window.open(`https://maps.google.com/?q=+${address}`)
   }
   whenSubmit(){
+    this.showBars=false
+    this.showSearch=false
+    this.error=''
     this._apiService.searchLocation(this.newPlace)
     .then((data)=>{
       if(data.response){
         if(data.response.statusCode){
           this.error="Please enter a correct location"
+          this.words=''
+          this.showSearch=true
+          console.log(this.showSearch)
           return
         }
       }
       else{
         this.places=data
+        this.words=''
         this.error=""
       }
-          this.show=true
+          this.showBars=true
+          this.showSearch=true
           this.places=data
     })
     .catch((errors)=>{
     })
   }
   getLocation(){
-    navigator.geolocation.getCurrentPosition((position)=> {
+    navigator.geolocation.getCurrentPosition((position) => {
       this.lat = position.coords.latitude;
       this.lng = position.coords.longitude;
       this.zoom = 14;
       this._apiService.searchGeoLocation({lat:this.lat,long:this.lng})
       .then((data)=>{
-        this.show=true
+        this.showBars=true
+        this.showSearch=true
         this.places=data
         this.error=""
       })
