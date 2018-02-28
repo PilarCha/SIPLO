3aedb4fa88243d229a5144f1ee104db82e185090
Pilar Chavez <pchavez92@gmail.com>
2018-02-27 20:22:27 -0800

    added lil documentation here and there

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MapsComponent} from './maps/maps.component'
 import {AppComponent} from './app.component'
 
 const routes: Routes = [
--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -15,10 +15,10 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
       })),
 
       state('large', style({
-        transform: 'scale(1.2)',
+        transform: 'scale(1.1)',
       })),
 
-      transition('small => large', animate('300ms easi-in')),
+      transition('small <=> large', animate('300ms ease-in')),
     ]),
   ]
 })
@@ -33,6 +33,9 @@ export class AppComponent {
   lat:number;
   lng:number;
 
+  //animation
+  state: string = 'small'
+
   constructor(private _router:Router, private _apiService:ApiService){
     this.newPlace={
       location:''
@@ -46,14 +49,22 @@ export class AppComponent {
     this.getword()
     this.getLocation()
   }
+
+  //hover over bars will increase the size
+  mouseOverAnimation() {
+    this.state = (this.state === 'small' ? 'large' : 'small')
+  }
+  //Picks a random sentence from an array
   getword(){
     var words=["Lips that touch liquor touch other lips quicker…","I drink to make other people more interesting.", "Everybody has to believe in something. I believe I’ll have another drink.", "You picked the wrong time to quit drinking", "It's always happy hour somewhere in the world", "This one is on the house"]
     this.words=words[Math.floor(Math.random()*(words.length))]
   }
+  //onClick opens a new window to the clicked location
   onClick(object){
     var address=object.location[0]+object.location[1]+object.name
     window.open(`https://maps.google.com/?q=+${address}`)
   }
+  //when the search bar is clicked starts up the reanimation
   whenSubmit(){
     this.words=''
     this.showBars=false
@@ -85,9 +96,11 @@ export class AppComponent {
     .catch((errors)=>{
     })
   }
+  //scrolls to top of the window after searching or rellocating
   scrollToTop (x,y) {
     window.scrollTo(x, y);
   }
+  //gets users geolocation using Googles API
   getLocation(){
     this.showBars = false;
     this.showSearch = false
